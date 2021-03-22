605b8fa990f9bd48e3c1b73ef82e16257256b66b
NatanMejirovsky <natan.mejirovsky@kryonsystems.com>
2021-03-22 22:03:43 +0200

    organize files

--- blogs-api/lib/cache/cache.js
+++ blogs-api/lib/cache/cache.js
@@ -4,10 +4,6 @@ let client;
 
 const init = async (conf)=>{
     client = new Redis(conf.host);
-      
-    await client.set("foo", "bar");
-
-    let res = await client.get("foo");
 };
 
 module.exports = {
--- blogs-api/lib/server.js
+++ blogs-api/lib/server.js
@@ -70,11 +70,12 @@ const verify_token = async (req, res, next) => {
 let startprom = new Promise(async (resolve, reject) => {
 	try {
 		const env = process.NODE_ENV || 'dev';
-		await tokenVerifier.init(require(`./config/${env}/jwt.json`));
-		await logger.init(require(`./config/${env}/logger.json`));
-		await blogsDb.init(require(`./config/${env}/elastic_srch.json`));
-		await authorsDb.init(require(`./config/${env}/dbconf.json`));
-		await cache.init(require(`./config/${env}/cache.json`))
+		
+		await tokenVerifier.init(require(`../config/${env}/jwt.json`));
+		await logger.init(require(`../config/${env}/logger.json`));
+		await blogsDb.init(require(`../config/${env}/elastic_srch.json`));
+		await authorsDb.init(require(`../config/${env}/dbconf.json`));
+		await cache.init(require(`../config/${env}/cache.json`))
 		resolve();
 	} catch (error) {
 		reject(error);
