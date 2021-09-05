e6509e0de0fee0c7c90161e22e02bde97f14de1c
Natan Mejirovsky <natan.mejirovsky@kryonsystems.com>
2021-09-05 21:34:59 +0300

    swagger better

--- blogs-api/lib/routes/blogs.js
+++ blogs-api/lib/routes/blogs.js
@@ -87,9 +87,13 @@
  *          blogId:
  *            type: string
 */
+
+const express = require('express'),
+     logger = require('../util/logger'),
+     router = express.Router(),
+	 blogsService = require('../services/blogs_service');
 /**
  * @swagger
- * paths:
  *  /blogs/:
  *    get:
  *      summary: Lists all the blogs
@@ -108,29 +112,20 @@
  *            application/json:
  *              schema:
  *                $ref: '#/components/schemas/Blog'
- *    post:
- *      summary: Creates a new blog
- *      parameters:
- *       - in: header
- *         name: authorization
- *         schema:
- *           type: string
- *           format: Bearer <JWT token base64 string from keycloak>
- *         required: true
- *      tags: [Blogs]
- *      requestBody:
- *        required: true
- *        content:
- *          application/json:
- *            schema:
- *              $ref: '#/components/schemas/BlogAddRequest'
- *      responses:
- *        "201":
- *          description: The id of created blog.
- *          content:
- *            application/json:
- *              schema:
- *                $ref: '#/components/schemas/BlogAddResponse'
+ */
+router.get('/', async (req, res) => {
+	try {
+		const blogs = await blogsService.getAllBlogs(req.query.count);
+		if (blogs)
+			return res.status(200).json(blogs);
+	} catch (error) {
+		logger.getLoggger().error(error);
+		return res.status(500).send('Error on getting blogs');
+	}
+});
+
+/**
+ * @swagger
  *  /blogs/{id}:
  *    get:
  *      summary: Gets a blog by id
@@ -158,6 +153,74 @@
  *                $ref: '#/components/schemas/BlogNoId'
  *        "404":
  *          description: Blog not found.
+ */
+router.get('/:id', async (req, res) => {
+    try {
+		const blog = await blogsService.getBlogById(req.params.id);
+		if(blog.state){
+			return res.status(blog.state).send(blog.msg || 'error');
+		}
+		if (blog)
+			return res.status(200).json(blog);
+	} catch (error) {
+		logger.getLoggger().error(error);
+		return res.status(500).send('Error on getting blog');
+	}
+});
+
+/**
+ * @swagger
+ *  /blogs/:
+ *    post:
+ *      summary: Creates a new blog
+ *      parameters:
+ *       - in: header
+ *         name: authorization
+ *         schema:
+ *           type: string
+ *           format: Bearer <JWT token base64 string from keycloak>
+ *         required: true
+ *      tags: [Blogs]
+ *      requestBody:
+ *        required: true
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/BlogAddRequest'
+ *      responses:
+ *        "201":
+ *          description: The id of created blog.
+ *          content:
+ *            application/json:
+ *              schema:
+ *                $ref: '#/components/schemas/BlogAddResponse'
+ */
+router.post('/', async (req, res) => {
+	try {
+		const { title,  content } = req.body;
+		const result = await blogsService.addBlog({ title,  content }, req.userContext);
+
+		if(result.state){
+			if(result.msg){
+				return res.status(result.state).send(result.msg);
+			}
+			else{
+				
+				if(result.state === 401)
+					return res.status(401).send('User is not authorized to post blogs or not registered in blogs application');
+			}
+		}
+		
+
+		return res.status(201).json({ blogId: result });
+	} catch (error) {
+		logger.getLoggger().error(error);
+		return res.status(500).send('Error on adding blog');
+	}
+});
+
+/**
+ * @swagger
  *    put:
  *      summary: Updates a blog
  *      tags: [Blogs]
@@ -186,84 +249,7 @@
  *          description: Update was successful.
  *        "404":
  *          description: Blog not found.
- *    delete:
- *      summary: Deletes a blog by id
- *      tags: [Blogs]
- *      parameters:
- *       - in: header
- *         name: authorization
- *         schema:
- *           type: string
- *           format: Bearer <JWT token base64 string from keycloak>
- *         required: true
- *         description: JWT token
- *       - in: path
- *         name: id
- *         schema:
- *           type: string
- *         required: true
- *         description: The blog id
- *      responses:
- *        "202":
- *          description: Delete was successful.
- *        "404":
- *          description: Blog not found.
  */
-
-const express = require('express'),
-     logger = require('../util/logger'),
-     router = express.Router(),
-	 blogsService = require('../services/blogs_service');
-
-router.get('/', async (req, res) => {
-	try {
-		const blogs = await blogsService.getAllBlogs(req.query.count);
-		if (blogs)
-			return res.status(200).json(blogs);
-	} catch (error) {
-		logger.getLoggger().error(error);
-		return res.status(500).send('Error on getting blogs');
-	}
-});
-
-router.get('/:id', async (req, res) => {
-    try {
-		const blog = await blogsService.getBlogById(req.params.id);
-		if(blog.state){
-			return res.status(blog.state).send(blog.msg || 'error');
-		}
-		if (blog)
-			return res.status(200).json(blog);
-	} catch (error) {
-		logger.getLoggger().error(error);
-		return res.status(500).send('Error on getting blog');
-	}
-});
-
-router.post('/', async (req, res) => {
-	try {
-		const { title,  content } = req.body;
-		const result = await blogsService.addBlog({ title,  content }, req.userContext);
-
-		if(result.state){
-			if(result.msg){
-				return res.status(result.state).send(result.msg);
-			}
-			else{
-				
-				if(result.state === 401)
-					return res.status(401).send('User is not authorized to post blogs or not registered in blogs application');
-			}
-		}
-		
-
-		return res.status(201).json({ blogId: result });
-	} catch (error) {
-		logger.getLoggger().error(error);
-		return res.status(500).send('Error on adding blog');
-	}
-});
-
 router.put('/:id', async (req, res) => {
 	try {
 		const { title, content } = req.body;
@@ -288,6 +274,31 @@ router.put('/:id', async (req, res) => {
 	}
 });
 
+/**
+ * @swagger
+ *    delete:
+ *      summary: Deletes a blog by id
+ *      tags: [Blogs]
+ *      parameters:
+ *       - in: header
+ *         name: authorization
+ *         schema:
+ *           type: string
+ *           format: Bearer <JWT token base64 string from keycloak>
+ *         required: true
+ *         description: JWT token
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The blog id
+ *      responses:
+ *        "202":
+ *          description: Delete was successful.
+ *        "404":
+ *          description: Blog not found.
+ */
 router.delete('/:id', async (req, res) => {
     try {
 		const result = await blogsService.removeBlog(req.params.id, req.userContext)
